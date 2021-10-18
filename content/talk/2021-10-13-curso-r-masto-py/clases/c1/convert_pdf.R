4e94618190757193b91ca8edd256c781306a3798
Marília Melo Favalesso <mariliabioufpr@gmail.com>
2021-10-18 16:36:12 -0300

    gitignore convert pdf

--- content/talk/2021-10-13-curso-r-masto-py/clases/c1/convert_pdf.R
+++ content/talk/2021-10-13-curso-r-masto-py/clases/c1/convert_pdf.R
@@ -10,7 +10,7 @@ library(xaringan)
 library(tidyverse)
 
 # convert to pdf
-setwd("C:/Users/mmfav/OneDrive/GitHub/cursos/somadosquadrados/introduccionalR/clase_1")
+setwd("C:/Users/mmfav/OneDrive/GitHub/soma quadrados/site_soma/content/talk/2021-10-13-curso-r-masto-py/clases/c1")
 purrr::map(dir(pattern = ".Rmd$"), pagedown::chrome_print(timeout = 120,  options = list(transferMode = "ReturnAsStream")))
 
 pagedown::chrome_print("clase_1.html", timeout = 120,  options = list(transferMode = "ReturnAsStream"))
